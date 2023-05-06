efbff39b8b9343556f04090718283365b3f6d7f7
sdziuda <szymondziuda95@gmail.com>
2023-05-06 23:32:26 +0200

    Improved style
    Moved import to the top, added new line at the end

--- se_med_browser/se_med_browser/settings.py
+++ se_med_browser/se_med_browser/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,5 +131,4 @@ STATIC_ROOT = BASE_DIR/'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import django_on_heroku
 django_on_heroku.settings(locals())
