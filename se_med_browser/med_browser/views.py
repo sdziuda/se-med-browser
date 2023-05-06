f6d426ebfe69d3e34605266aba949f6d6af29b01
sdziuda <szymondziuda95@gmail.com>
2023-05-07 00:16:01 +0200

    Added search result generation

--- se_med_browser/med_browser/views.py
+++ se_med_browser/med_browser/views.py
@@ -6,18 +6,28 @@ from .models import Medicine
 class SearchForm(forms.Form):
     phrase = forms.CharField(max_length=2000,
                              label='',
-                             widget=forms.TextInput(attrs={'placeholder': 'Wpisz wyszukiwaną frazę'}))
+                             widget=forms.TextInput(attrs={'placeholder': 'Wpisz wyszukiwaną frazę'}),
+                             required=False)
 
 
 def index(request):
-    med = Medicine.objects.all()[:25]
     if request.method == 'POST':
         if 'search.x' in request.POST:  # why 'search.x'? and why not 'search'? idk tbh, search doesn't work
             form = SearchForm(request.POST)
             if form.is_valid():
                 phrase = form.cleaned_data['phrase']
-                return render(request, 'index.html', {'phrase': phrase, 'search_form': form, 'med': med})
+                if phrase == '':
+                    return render(request, 'index.html', {'search_form': form, 'search': True})
+
+                med = Medicine.objects.filter(name__icontains=phrase)
+                med = med.union(Medicine.objects.filter(active_substance__name__icontains=phrase))
+                med = med.union(Medicine.objects.filter(GTIN_number__icontains=phrase))
+                med = med.union(Medicine.objects.filter(form__icontains=phrase))
+                med = med.union(Medicine.objects.filter(dose__icontains=phrase))
+                med = med.union(Medicine.objects.filter(package_contents__icontains=phrase))
+                med = med.order_by('name', 'form', 'dose', 'package_contents')
+                return render(request, 'index.html', {'search_form': form, 'med': med, 'search': True})
 
     search_form = SearchForm()
-    return render(request, 'index.html', {'search_form': search_form, 'med': med})
+    return render(request, 'index.html', {'search_form': search_form, 'search': False})
 
