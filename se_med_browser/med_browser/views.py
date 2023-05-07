d137ac4affbc2c525c10056cd2f6c2e8a62308cd
sdziuda <szymondziuda95@gmail.com>
2023-05-07 14:35:46 +0200

    Added dropdown list to limit number of results
    Initially the number of results is set to 25

--- se_med_browser/med_browser/views.py
+++ se_med_browser/med_browser/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django import forms
+from django.core.cache import cache
 from .models import Medicine
 
 
@@ -8,6 +9,14 @@ class SearchForm(forms.Form):
                              label='',
                              widget=forms.TextInput(attrs={'placeholder': 'Wpisz wyszukiwaną frazę'}),
                              required=False)
+
+
+class TopForm(forms.Form):
+    top = forms.ChoiceField(choices=[(5, 5), (10, 10), (25, 25), (50, 50), (100, 100), ('all', 'Wszystkie')],
+                            label='Pokaż',
+                            initial=25,
+                            required=False,
+                            widget=forms.Select(attrs={'onchange': 'this.form.submit()'}))
 
 
 def index(request):
@@ -26,7 +35,28 @@ def index(request):
                 med = med.union(Medicine.objects.filter(dose__icontains=phrase))
                 med = med.union(Medicine.objects.filter(package_contents__icontains=phrase))
                 med = med.order_by('name', 'form', 'dose', 'package_contents')
-                return render(request, 'index.html', {'search_form': form, 'med': med, 'search': True})
+                top_form = TopForm()
+                cache.set('med', med)
+                cache.set('phrase', phrase)
+                return render(request, 'index.html', {'search_form': form, 'med': med[:25], 'search': True,
+                                                      'top_form': top_form})
+        elif 'top' in request.POST:
+            med = cache.get('med')
+            if med is None:
+                med = []
+            phrase = cache.get('phrase')
+            if phrase is None:
+                phrase = ''
+            top_form = TopForm(request.POST)
+            if top_form.is_valid():
+                top = top_form.cleaned_data['top']
+                search_form = SearchForm(initial={'phrase': phrase})
+                context = {'search_form': search_form, 'search': True, 'top_form': top_form}
+                if top == 'all':
+                    context['med'] = med
+                else:
+                    context['med'] = med[:int(top)]
+                return render(request, 'index.html', context)
 
     search_form = SearchForm()
     return render(request, 'index.html', {'search_form': search_form, 'search': False})
